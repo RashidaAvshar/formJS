46e27fcf96542f85e92857882e342f27c0a1cb96
RashidaAvshar <rashidaavshar@gmail.com>
2023-02-01 11:31:38 +0400

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,68 @@
+// document.querySelector("#register").addEventListener("click", ()=>{
+//     let username = document.querySelector("#name").value;
+//     let mail = document.querySelector("#email").value;
+//     let password = document.querySelector("#password").value;
+//     let confirm = document.querySelector("#again").value;
+//     let errorMessage = document.querySelector("#error-message");
+
+//     if(password !== confirm){
+//     errorMessage.style.color = "red";
+//     errorMessage.innerHTML = "Şifrə düz deyil"
+// }else{
+//     errorMessage.style.color = "green";
+//     errorMessage.innerHTML = "Şifrə uyğundur"
+//     let data = {
+//         username,
+//         mail,
+//         password,
+//         confirm,
+//     }
+//     console.log(data);
+//     username = "";
+//     mail = "";
+//     password = "";
+//     confirm =  "";
+// }
+// })
+
+// !========================minimum 8 simvol
+
 document.querySelector("#register").addEventListener("click", ()=>{
-    let username = document.querySelector("#username").value;
+    let username = document.querySelector("#name").value;
     let mail = document.querySelector("#email").value;
     let password = document.querySelector("#password").value;
     let confirm = document.querySelector("#again").value;
+    let errorMessage = document.querySelector("#error-message");
 
+    if(password.length<8){
+        errorMessage.style.color = "red";
+            errorMessage.innerHTML = "Minimum 8 simvol"
+    } else   
     if(password !== confirm){
-    alert("sifre duz deil")
-}else{
-    alert("sifre duzdur")
+            errorMessage.style.color = "red";
+            errorMessage.innerHTML = "Şifrə düz deyil"
+        }else{
+            errorMessage.style.color = "green";
+            errorMessage.innerHTML = "Şifrə uyğundur"
+    let data = {
+        username,
+        mail,
+        password,
+        confirm,
+    }
+    console.log(data);
+    document.querySelector("#name").value = "";
+    document.querySelector("#email").value = "";
+    document.querySelector("#password").value = "";
+    document.querySelector("#again").value = "";
 }
+})
+
+password.addEventListener("keyup", ()=>{
+    let password =document.querySelector ("#password");
+    if (password.value.length >= 8){
+        password.style.border = "10px solid green"
+    }else{
+        password.style.border = "10px solid red"
+    }
 })
